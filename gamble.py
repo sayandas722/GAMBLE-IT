43f6600fa5fb77b5339a96c14d521be34ecb4158
SAYAN DAS <73294479+sayandas722@users.noreply.github.com>
2021-04-14 09:55:14 +0530

    CODE

---
+++ gamble.py
@@ -0,0 +1,21 @@
+import random
+import matplotlib.pyplot as plt
+
+account = 0
+x=[]
+y=[]
+for i in range(365):
+    x.append(i+1)
+    bet= random.randint(1,10)
+    lucky_draw=random.randint(1,10)
+    #print("Bet:",bet)
+    #print("Lucky draw:",lucky_draw)
+
+    if bet == lucky_draw:
+        account=account+900-100
+    else:
+        account= account-100
+    y.append(account)
+print("Amount in your game account:" ,account)
+plt.plot(x,y)
+plt.show()
